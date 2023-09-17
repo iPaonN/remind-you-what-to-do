d5c496c6adb810bf6f7f7221d9937d350ee00f7e
Panot Liangpiboon <138891084+iPaonN@users.noreply.github.com>
2023-09-17 14:23:06 +0700

    update some description
    add some description

--- main.py
+++ main.py
@@ -3,20 +3,23 @@
 import discord
 from discord.ext import commands
 
+#เอาไว้สำหรับรอรับคำสั่ง
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 # Run the bot
 # always don't forget to remove token
+
 def main():
     """Bot"""
-    token = ''
+    token = '' #สำหรับรันบอท
 
-    @bot.event
+    @bot.event #ดูสถานะ
     async def on_ready():
         """standby"""
         print("Hi, This is Yu! I'm here")
         print("<---------------------->")
 
+    #รอคำสั่ง
     @bot.command()
     async def hello(ctx):
         """hi"""
