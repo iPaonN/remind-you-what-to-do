adc54c4e9888714980fdeed209ae459ac99a4080
Panot Liangpiboon <138891084+iPaonN@users.noreply.github.com>
2023-11-10 15:44:09 +0700

    fix and clean some codes add clear list
    Co-Authored-By: Thanasit Tungjai <141387234+66070083@users.noreply.github.com>

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ client = discord.Client(intents=discord.Intents.all())
 slash = bot.create_group('knock', 'What do you want me to remind about?')
 
 class Queue:
-    """Queue"""
+    """Queue, thanks to mickey083 for idea"""
     def __init__(self):
         """a list"""
         self._queue = []
@@ -38,6 +38,10 @@ class Queue:
             return "The queue is empty."
         return '\n'.join([f'{i + 1}. {s}' for i, s in enumerate(self._queue[::-1])])
 
+    def dequeue(self):
+        """clear all set tasks"""
+        self._queue.clear()
+
 
 my_queue = Queue()
 
@@ -50,36 +54,32 @@ async def on_ready():
     print("--> Standing by... READY!")
 
 
-# รอคำสั่ง
-@bot.command()
-async def hello(ctx):
-    """hi"""
-    await ctx.send("Hi, I am here!")
-
-
 # ลอง Basic Reminds - 66070105
 # Basic Reminds
 @bot.command()
 async def knock(ctx, time, *task):
+    """Basic Reminds"""
 
     task = ' '.join(task)
     converted_time = convert(time)
 
     if converted_time == -1:
         await ctx.send(embed=discord.Embed(color=discord.Color.red(),
-                                           description=f"Wrong Time Format!")
-                                           )
+                                           description="Wrong Time Format!"
+                                           ))
         return
 
     if converted_time == -2:
         await ctx.send(embed=discord.Embed(color=discord.Color.red(),
-                                           description=f"The time must be an integer")
-                                           )
+                                           description="The time must be an integer"
+                                           ))
         return
 
+    my_queue.enqueue(task)
+
     await ctx.send(embed=discord.Embed(color=discord.Color.blue(),
-                                       description=f"I Will remind **\"{task}\"** in **{time}**.")
-                                       )
+                                       description=f"I will remind **\"{task}\"** in **{time}**."
+                                       ))
 
     await asyncio.sleep(converted_time)
     await ctx.send(f":alarm_clock: {ctx.author.mention} It's time for you to **\"{task}\"**")
@@ -90,10 +90,10 @@ async def knock(ctx, time, *task):
 async def knock_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(embed=discord.Embed(color=discord.Color.red(),
-                        description=f"To use this command you have to type \"!knock <time> <task>\" \n \
+                        description="To use this command you have to type \"!knock <time> <task>\" \n \
         Time Format : **s** as second, **m** as minute, **h** as hour, **d** as day \n \
-        Command Example : !knock 5s hello world")
-        )
+        Command Example : !knock 5s hello world or try slash commands!"
+        ))
 
 
 # slash commands
@@ -110,23 +110,25 @@ async def test(interaction: discord.Interaction):
 @slash.command(name="timezone", description='Continent/City')
 async def sl_timezone(interaction: discord.Interaction, continent_city: str):
     """Slash Command of Timezone"""
-    continent_city.replace(' ', '_')
+
+    continent_city = continent_city.replace(' ', '_')
+
     try:
         # Check if the provided timezone is valid
         tz = pytz.timezone(continent_city)
     except pytz.UnknownTimeZoneError:
         await interaction.response.send_message(embed=discord.Embed(color=discord.Color.red(),
-                                                                    description=f"Invalid timezone: {continent_city}\n"
-        "Please specify me, such as Europe/Vienna")
-        )
+                                                                    description=f"Invalid timezone: {continent_city.title()}\n"
+        "Please specify me, such as Europe/Vienna."))
         return
 
     # Get the current time in the specified timezone
     current_time = datetime.now(tz).strftime("**%Y-%m-%d %H:%M:%S**")
 
     await interaction.response.send_message(embed=discord.Embed(color=discord.Color.blue(),
-                                                                description=f"Current time in {continent_city}: {current_time}")
-                                                                )
+                                                                description=f"Current time in {continent_city.title()}: {current_time}",
+                                                                timestamp= datetime.now()
+                                                                ))
 
 
 @slash.command(name='me', description='I will remind you!')
@@ -138,14 +140,14 @@ async def sl_remind(interaction: discord.Interaction,time: str, task: str): #use
 
     if converted_time == -1:
         await interaction.response.send_message(embed=discord.Embed(color=discord.Color.red(),
-                                                                    description=f"Wrong Time Format!")
-                                                                    )
+                                                                    description="Wrong Time Format!"
+                                                                    ))
         return
 
     if converted_time == -2:
         await interaction.response.send_message(embed=discord.Embed(color=discord.Color.red(),
-                    description=f"The time must be an integer and unit of time such as s for second(s)")
-                    )
+                    description="The time must be an integer and unit of time such as s for second(s)"
+                    ))
         return
 
     my_queue.enqueue(task)
@@ -160,20 +162,38 @@ async def sl_remind(interaction: discord.Interaction,time: str, task: str): #use
 
 @slash.command(name='remind_list', description='Task List')
 async def queue(interaction: discord.Interaction):
-    '''hist'''
+    '''display the histor'''
 
     embed = discord.Embed(
-        title='Queue',
-        description= 'Here is the queue.',
+        title='Task List',
+        description= 'Here, all set tasks.',
         timestamp= datetime.now(),
         color=discord.Color.yellow()
     )
 
-    queue_list = my_queue.display_q()
+    embed.add_field(
+        name="Recently!",
+        value=my_queue.display_q(),
+        inline=False
+    )
+
+    await interaction.response.send_message(embed=embed)
+
+@slash.command(name='clear_tasks', description='Clear all set tasks')
+async def re_queue(interaction: discord.Interaction):
+    '''clear the histor'''
+
+    my_queue.dequeue()
+
+    embed = discord.Embed(
+        title='All set tasks has been cleared.',
+        timestamp= datetime.now(),
+        color=discord.Color.purple()
+    )
 
     embed.add_field(
-        name="Currently!",
-        value=queue_list,
+        name="All clear!",
+        value=my_queue.display_q(),
         inline=False
     )
 
